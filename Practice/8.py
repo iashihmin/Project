e90bcc3557b1b813530c98cb9c0b084ed471456e
«ваше <>
2015-06-09 22:11:16 +0300

    «Reload»

--- Practice/8.py
+++ Practice/8.py
@@ -9,12 +9,16 @@ class Block(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
 pygame.init()
+pygame.font.init()
 
+inf_spd = pygame.font.Font(None, 50)
 
 background = pygame.image.load('road.jpg')
 background_size = background.get_size()
 screen_width, screen_height = background_size
 screen = pygame.display.set_mode([screen_width, screen_height])
+
+info = pygame.Surface( (150, 50 ))
 
 x_speed = 0
 
@@ -48,17 +52,25 @@ while done:
                 speed.append(speed[-1]-1)
                 y_speed = speed[-1]
                 del speed[0]
-                
+                print(speed)
             elif event.key == pygame.K_DOWN:
                 speed.append(speed[-1]+1)
                 del speed[0]
                 
             elif event.key == pygame.K_LEFT:
                 if speed[-1] != 0:
-                    x_speed = -2
+                    if x_coord > -36:
+                        if speed[-1] < 50:
+                            x_speed = -2
+                        else:
+                            x_speed = 100/speed[-1]
             elif event.key == pygame.K_RIGHT:
                 if speed[-1] != 0:
-                    x_speed = 2
+                    if x_coord < 225:
+                        if speed[-1] < 50:
+                            x_speed = 2
+                        else:
+                            x_speed = - 100/speed[-1]
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 x_speed = 0
@@ -71,9 +83,14 @@ while done:
 
     y1 -= speed[-1]
     y -= speed[-1]
+
+    info.fill(BLACK)
+
     screen.blit(background, (x,y))
     screen.blit(background, (x,y1))
-
+    screen.blit(info, (250,0) )
+    screen.blit(inf_spd.render(str(-speed[2]*2) + 'km/h', 1, WHITE), (250,5) )
+                  
     if y > screen_height:
         y = - screen_height
     if y1 > screen_height:
@@ -84,6 +101,9 @@ while done:
 
     x_coord += x_speed
 
+    if x_coord > 225 or x_coord < -36:
+        x_speed = 0
+
           
     sprites.draw(screen)
 
@@ -92,5 +112,5 @@ while done:
     pygame.display.flip()
     pygame.display.update()
 
-
+pygame.font.quit()
 pygame.quit()
