3297f1f382cfe6e8cb8cf3b1801cc47b5c70af2a
«ваше <>
2015-06-12 01:26:14 +0300

    Reload

--- Practice/8.py
+++ Practice/8.py
@@ -61,7 +61,7 @@ screen = pygame.display.set_mode([screen_width, screen_height])
 ''' player '''
 x_speed = 0
 speed = [0, 0, 0]
-x_coord = 225
+x_coord = 235
 y_coord = 580
 x = 0
 y = 0
@@ -69,17 +69,22 @@ y1 = -screen_height
 ''' --- '''
 ''' bot '''
 by_spd = 0
-bx_crd = 225
+bot_real_speed = -60
+bx_crd = 235
 by_crd = 300
 ''' --- '''
 
 sprites = pygame.sprite.Group()
 player = Block('car.png')
-#bot = Block('car2.png')
+prect = pygame.image.load('car.png').convert().get_rect()
+bot = Block('car2.png')
+#print(pygame.image.load('car.png').get_size())
+brect = pygame.image.load('car2.png').convert().get_rect()
 sprites.add(player)
-#sprites.add(bot)
+sprites.add(bot)
 
 done = True
+
 
 clock = pygame.time.Clock()
 pygame.mouse.set_visible(0)
@@ -91,7 +96,9 @@ while done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = False
+            
         if event.type == pygame.KEYDOWN:
+            '''
             if event.key == pygame.K_UP:
                 speed.append(speed[-1]-1)
                 y_speed = speed[-1]
@@ -99,14 +106,13 @@ while done:
                 
             elif event.key == pygame.K_DOWN:
                 speed.append(speed[-1]+1)
-                del speed[0]
+                del speed[0]'''
                 
-            elif event.key == pygame.K_LEFT:
+            if event.key == pygame.K_LEFT:
                 if speed[-1] != 0:
                     if x_coord > 10:
                         if speed[-1] > -50:
                             x_speed = -2
-
                         else:
                             x_speed = 100/speed[-1]
 
@@ -123,44 +129,73 @@ while done:
             elif event.key == pygame.K_RIGHT:
                 x_speed = 0
 
+    
 
+        
+    '''player and bot'''
+    if (by_crd+199) > (y_coord - 80) and bx_crd < (x_coord + 180) and by_crd < 800 and x_coord > 50:
+        if x_coord > 10:
+            if speed[-1] > -50:
+                x_speed = -2
+            else:
+                x_speed = 100/speed[-1]
+    elif bx_crd > (x_coord+180) and (by_crd+199) > (y_coord - 80):
+        if speed[-1] != 0:
+            if x_coord < 275:
+                if speed[-1] > -50:
+                    x_speed = 2
+                else:
+                    x_speed = - 100/speed[-1]        
+
+        #speed[-1] += ((by_crd+199)-(y_coord-80))/(-speed[-1]+2)
+
+        
     if speed[-1] > 0:
         speed[-1] = 0
+        
+    else:
+        if -speed[-1]*2 < priority:
+            speed[-1] = speed[-1] - acceleration
+            #print(speed[-1])
+    '''---'''
 
-    if -speed[-1]*2 < priority:
-        speed[-1] = speed[-1] - acceleration
-        #print(speed[-1])
+    '''bot speed'''
+    by_spd = - (bot_real_speed - speed[-1]*2) / 30
+    by_crd -= by_spd
+    '''---'''
 
     y1 -= speed[-1]
     y -= speed[-1]
 
-    #info.fill(BLACK)
-
-    screen.blit(background, (x,y))
-    screen.blit(background, (x,y1))
-    #screen.blit(info, (250,0) )
-    screen.blit(inf_spd.render(str(int(-speed[-1]*2)) + 'km/h', 1, WHITE), (200,5) )
-                  
+    if by_crd > screen_height:
+        by_crd = - screen_height
     if y > screen_height:
         y = - screen_height
     if y1 > screen_height:
         y1 = - screen_height
 
-    by_crd -= speed[-1]  
-        
+    
+    
+
     player.rect.x = x_coord
     player.rect.y = y_coord
+    
 
-    #bot.rect.x = bx_crd
-    #bot.rect.y = by_crd
+    bot.rect.x = bx_crd
+    bot.rect.y = by_crd
 
     x_coord += x_speed
 
-    if x_coord > 275 or x_coord < 10:
+    if x_coord > 235 or x_coord < 45:
         x_speed = 0
 
           
+    
+    screen.blit(background, (x,y))
+    screen.blit(background, (x,y1))
+    #screen.blit(info, (250,0) )
     sprites.draw(screen)
+    screen.blit(inf_spd.render(str(int(-speed[-1]*2)) + 'km/h', 1, WHITE), (200,5) )
 
     clock.tick(60)
 
