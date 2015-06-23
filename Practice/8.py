cb2f3747ab0993f8745d2b319c00b4b47a8ac578
«ваше <>
2015-06-23 22:04:50 +0300

    Final

--- Practice/8.py
+++ Practice/8.py
@@ -87,12 +87,12 @@ y = 0
 y1 = -screen_height
 
 by_spd = 0
-bot_real_speed = -75
+bot_real_speed = -70
 bx_crd = 235
 by_crd = -199
 
 by_spd1 = 0
-bot_real_speed1 = -60
+bot_real_speed1 = -50
 bx_crd1 = 45
 by_crd1 = -199
 
@@ -110,18 +110,21 @@ def double_overtaking(py_crd, by_crd, speed_of_convergence, b_spd, b1_spd, px_cr
             if b_spd < b1_spd:
                 if distance1 <= distance2:
                     if s_counts >= f_counts:
-                        #print(1)
                         if py_spd*2 < b_spd:
                             py_spd = py_spd + acceleration
                         if py_spd*2 > b_spd:
                             py_spd = py_spd - acceleration
+                    else:
+                        if -py_spd*2 < priority:
+                            py_spd = py_spd - acceleration
+                        if -py_spd*2 > priority:
+                            py_spd = py_spd + acceleration
                 else:
                     c1 = (distance2 + 450)/(-(b_spd+priority*2)/30)
                     c2 = (distance1 - distance2 - 450)/(-(b1_spd+priority*2)/30)
                     if distance2 < 200:
                         if c1 > c2:
                             if px_crd > 45:
-                                #print(2)
                                 l_spd = x_spd
                                 if -py_spd*2 < priority:
                                     py_spd = py_spd - acceleration
@@ -131,55 +134,44 @@ def double_overtaking(py_crd, by_crd, speed_of_convergence, b_spd, b1_spd, px_cr
                         else:
                             s_counts = math.fabs((py_spd - b_spd) / acceleration)
                             f_counts = (distance / acceleration)
-                            #print('ddfd')
                             if s_counts >= f_counts:
                                 if (px_crd >= 234 and px_crd <= 236) and by_crd < 800:
-                                    #print(3)
                                     if py_spd*2 < b_spd:
                                         py_spd = py_spd + acceleration
                                     if py_spd*2 > b_spd:
                                         py_spd = py_spd - acceleration
                     else:
-                        #print(4)
                         if -py_spd*2 < priority:
                             py_spd = py_spd - acceleration
                         if -py_spd*2 > priority:
                             py_spd = py_spd + acceleration
                    
             if b_spd > b1_spd:
-                #print('a')
                 if distance1 >= distance2:
-                    #print('b')
                     s_counts = math.fabs((py_spd - b_spd) / acceleration)
                     f_counts = (distance / acceleration)
                     if s_counts >= f_counts:
-                        #print(px_crd)
                         if (px_crd >= 234 and px_crd <= 236) and by_crd < 800:
-                            #print(5)
                             if py_spd*2 < b_spd:
                                 py_spd = py_spd + acceleration
                             if py_spd*2 > b_spd:
                                 py_spd = py_spd - acceleration
-                            if py_spd*2 > b_spd-3 and py_spd*2 < b_spd:
+                            if py_spd*2 > b_spd-3 and py_spd*2 <= b_spd+1:
                                 l_spd = x_spd
                         if px_crd >= 45 and px_crd < 235:
-                            #print('c')
                             if px_crd >= 44 and px_crd <= 46:
                                 if distance1 < 200:
-                                    #print(6)
                                     if py_spd*2 < b1_spd:
                                         py_spd = py_spd + acceleration  
                                     if py_spd*2 > b1_spd:
                                         py_spd = py_spd - acceleration
                                 else:
-                                    #print(7)
                                     if -py_spd*2 < priority:
                                         py_spd = py_spd - acceleration
                                     if -py_spd*2 > priority:
                                         py_spd = py_spd + acceleration
                             else:
                                 if distance1 >= 200:
-                                    #print(8)
                                     if -py_spd*2 < priority:
                                         py_spd = py_spd - acceleration
                                     if -py_spd*2 > priority:
@@ -188,9 +180,7 @@ def double_overtaking(py_crd, by_crd, speed_of_convergence, b_spd, b1_spd, px_cr
                                     l_spd = x_spd
 
                     else:
-                        #print(13)
                         if distance2 < 200:
-                            #print(12)
                             if py_spd*2 < b_spd:
                                 py_spd = py_spd + acceleration
                             if py_spd*2 > b_spd:
@@ -200,32 +190,28 @@ def double_overtaking(py_crd, by_crd, speed_of_convergence, b_spd, b1_spd, px_cr
                         if -py_spd*2 > priority:
                             py_spd = py_spd + acceleration
                 else:
-                    #print(9)
                     if px_crd < 235:
                         l_spd = -x_spd
                     s_counts = math.fabs((py_spd - b_spd) / acceleration)
                     f_counts = (distance / acceleration)
                     if s_counts >= f_counts:
                         if px_crd == 235:
-                            #print(10)
                             if py_spd*2 < b_spd:
                                 py_spd = py_spd + acceleration
                             if py_spd*2 > b_spd:
                                 py_spd = py_spd - acceleration
                     if distance2 < 200:
-                        #print('very very very very very very very very very very very very very very very very very very long string')
                         if py_spd*2 < b_spd:
                             py_spd = py_spd + acceleration
                         if py_spd*2 > b_spd:
                             py_spd = py_spd - acceleration
     else:
-       # print(11)
         if px_crd < 235:
             l_spd = -x_spd
         if -py_spd*2 < priority:
             py_spd = py_spd - acceleration
         if -py_spd*2 > priority:
-            py_spd = py_spd + acceleration*2
+            py_spd = py_spd + acceleration
     mas.append(l_spd)
     mas.append(py_spd)
     return mas 
@@ -250,36 +236,31 @@ while done:
         if event.type == pygame.QUIT:
             done = False
 
+    end = -priority + 10
+    start = end + 20
+    
     if by_crd > 800:
         by_crd = random.uniform(-screen_height, -screen_height*(0.5))
-        bot_real_speed = random.randint(-90, -70)
+        bot_real_speed = random.randint(end, start)
     if by_crd1 > 800:
         by_crd1 = random.uniform(-4*screen_height, -3*screen_height)
-        bot_real_speed1 = random.randint(-90,-70)
+        bot_real_speed1 = random.randint(end, start)
         if bot_real_speed1 < (bot_real_speed + 5) and bot_real_speed1 > (bot_real_speed - 5):
             bot_real_speed1 = bot_real_speed-10
-            if bot_real_speed1 < -90:
+            if bot_real_speed1 < start:
                bot_real_speed1 = bot_real_speed + 10
-
-    if priority < 85:    
-        if -priority > (bot_real_speed + 5) or -priority < (bot_real_speed - 5):
-            if -priority <= -80:
-                bot_real_speed = -priority + 10
-        if -priority > (bot_real_speed1 + 5) or -priority < (bot_real_speed1 - 5):
-            if -priority <= -80:
-                bot_real_speed1 = -priority + 5
 
     if bot_real_speed1 < (bot_real_speed + 5) and bot_real_speed1 > (bot_real_speed - 5):
         bot_real_speed1 = bot_real_speed-10
-        if bot_real_speed1 < -90:
+        if bot_real_speed1 < start:
             bot_real_speed1 = bot_real_speed + 10
-        
+
+
+    
     if speed[-1] > -50:
         x_speed = -1
     else:
         x_speed = 50/speed[-1]
-
-    #print(bot_real_speed1, bot_real_speed)
     
     by_spd = - (bot_real_speed - speed[-1]*2) / 30
     by_crd -= by_spd
@@ -295,8 +276,6 @@ while done:
            
     y1 -= speed[-1]/(1.4)
     y -= speed[-1]/(1.4)
-    #if by_crd > screen_height:
-     #   by_crd = - screen_height
     if y > screen_height:
         y = - screen_height
     if y1 > screen_height:
